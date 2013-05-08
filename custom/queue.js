84a4bd813ae6da230f05a4cbbbb9999bf78fc586
Philip I. Thomas <mail@philipithomas.com>
2013-05-08 02:38:18 -0500

    Basic working program finished

--- custom/queue.js
+++ custom/queue.js
@@ -17,7 +17,7 @@ $("#simulate").click(function() {
 		mu = 1/service_input;
 		$("#arrival_display").text(arrival_input);
 		$("#service_display").text(service_input);
-
+		suggestion = 0;
 		for (c=1;c<=8;c++)
 		{ 		
 			// Reset rows
@@ -63,7 +63,7 @@ $("#simulate").click(function() {
 				$("#W" + c).text('');
 			}
 			else {
-				$("#rho" + c).text(Math.round( rho * 100) + "%");
+				$("#rho" + c).html(Math.round( rho * 100) + "%");
 				$("#L" + c).text(Math.round( Lq * 100)/100);
 				min = Math.floor( w );
 				sec = Math.floor(w%1*60);
@@ -75,15 +75,31 @@ $("#simulate").click(function() {
 					sec = "00";
 				}
 
+				$("#dumpbody"+c).html("<div>Servers: " + c + "</div><div>Arrival rate: " + lambda + "</div><div>Service Rate of one server:" + mu + "</div><div>Server utilization: " + rho + "</div><div>Steady-state probability of zero customers in system: " + Pnaught + "</div><div>Avg time in system per customer: " + w + " </div><div>Avg average time in queue per customer: " + wq + "</div><div>Avg number of customers in system: " + L + "</div><div>Avg number of customers in line: " + Lq + "</div>");
 				$("#W" + c).text( min+ ":" +sec );
 				if ( w > goal_service ) {
 					$("#row" + c).addClass('warning');
 				}
 				else {
-					$("#row" + c).addClass('success');					
+					$("#row" + c).addClass('success');	
+					if (suggestion == 0) {
+						suggestion = c;
+					}				
 				}
 			}
+			
+
 		}
+		// Now we print the suggestion
+			if (suggestion == 0 ) {
+				$("#rec_outer").addClass('hidden');
+
+			}else {
+				$("#rec_outer").removeClass('hidden');
+				$("#recommendation").text(suggestion);
+
+			}
+
 		$("#output").removeClass('hide');
 	}
 });
