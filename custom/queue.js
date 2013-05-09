b890bf1fe7363d4feda9d5ab0600961912002f52
Philip I. Thomas <mail@philipithomas.com>
2013-05-09 16:59:04 -0500

    Tweaking aesthetics

--- custom/queue.js
+++ custom/queue.js
@@ -61,7 +61,7 @@ $("#simulate").click(function() {
 				$("#rho" + c).text('');
 				$("#L" + c).text('');
 				$("#W" + c).text('');
-				$("#dumpbody"+c).html("Customers arrive faster than they may be served, so statistics are available. The line is always growing. ");
+				$("#dumpbody"+c).html("Customers arrive faster than they may be served, so statistics are unavailable. The line is always growing. ");
 
 			}
 			else {
